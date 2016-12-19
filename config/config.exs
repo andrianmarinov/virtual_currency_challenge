fefc0ca08a7b3ab97b83563db652d780e602b72f
andrianbmarinov <andrianbmarinov@gmail.com>
2016-12-19 22:32:41 +0000

    First try at Elixir

---
+++ config/config.exs
@@ -0,0 +1,11 @@
+use Mix.Config
+
+config :virtual_currency,
+	alphabet: "voyagerVOYAGER"
+
+#
+#     config :virtual_currency, key: :value
+#
+#
+#     Application.get_env(:virtual_currency, :key)
+#
